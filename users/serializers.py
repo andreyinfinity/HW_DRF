61623f332de3084128118fa0af577c9c408f083d
Andrey Potapov <aapot@ya.ru>
2024-02-21 10:20:14 +0300

    HW 24.3 fix register bug

--- users/serializers.py
+++ users/serializers.py
@@ -8,18 +8,18 @@ class UserSerializer(serializers.ModelSerializer):
     def get_payment_history(self, user):
         return [str(payment) for payment in user.payments_set.filter(user=user)]
 
-    def create(self, validated_data):
-        """Шифрование пароля при регистрации"""
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-    def update(self, user, validated_data):
-        """Шифрование пароля при обновлении профиля"""
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
+    # def create(self, validated_data):
+    #     """Шифрование пароля при регистрации"""
+    #     user = User(**validated_data)
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
+    #
+    # def update(self, user, validated_data):
+    #     """Шифрование пароля при обновлении профиля"""
+    #     user.set_password(validated_data['password'])
+    #     user.save()
+    #     return user
 
     class Meta:
         model = User
